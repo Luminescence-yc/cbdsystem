fb4e59b341fd2198446a4f685bafa0cfc3a49903
yc <yy888000@dingtalk.com>
2019-07-08 14:36:27 +0800

    idea修改代码

--- src/main/java/com/project/controller/DruidController.java
+++ src/main/java/com/project/controller/DruidController.java
@@ -1,15 +1,16 @@
 package com.project.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DruidController {
     public String git(){
         SimpleDateFormat simpleDateFormat=new SimpleDateFormat();
         System.out.println(simpleDateFormat.format(new Date()));
+        System.out.println("你好啊");
         return null;
     }
 }
