d36399bd6d61f2dcf005075fce7d659d056ca142
yc <yy888000@dingtalk.com>
2019-07-08 13:47:31 +0800

    修改contorller

--- src/main/java/com/project/controller/DruidController.java
+++ src/main/java/com/project/controller/DruidController.java
@@ -1,10 +1,13 @@
 package com.project.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+
 @RestController
 public class DruidController {
     public String git(){
-
+        SimpleDateFormat simpleDateFormat=new SimpleDateFormat();
         return null;
     }
 }
