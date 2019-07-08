cbadc14380cc6f307d9bedaf8e28a2e87e3b2d60
yc <yy888000@dingtalk.com>
2019-07-08 14:35:31 +0800

    eclipes修改代码

--- src/main/java/com/project/controller/DruidController.java
+++ src/main/java/com/project/controller/DruidController.java
@@ -10,6 +10,7 @@ public class DruidController {
     public String git(){
         SimpleDateFormat simpleDateFormat=new SimpleDateFormat();
         System.out.println(simpleDateFormat.format(new Date()));
+        System.out.println("我是eclipes");
         return null;
     }
 }
