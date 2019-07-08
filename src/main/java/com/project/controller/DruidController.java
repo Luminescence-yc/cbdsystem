fd9d81fd757e586eb4f10c7a682ed4cba2db9f1f
yc <yy888000@dingtalk.com>
2019-07-08 13:31:58 +0800

    修改contorller

--- src/main/java/com/project/controller/DruidController.java
+++ src/main/java/com/project/controller/DruidController.java
@@ -3,5 +3,8 @@ package com.project.controller;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DruidController {
+    public String git(){
 
+        return null;
+    }
 }
