080d4b7605d9d8a59f2da8efd948ed42f5a2bde8
yc <yy888000@dingtalk.com>
2019-07-08 14:28:56 +0800

    eclipes修改代码

--- src/main/java/com/project/controller/DruidController.java
+++ src/main/java/com/project/controller/DruidController.java
@@ -1,13 +1,15 @@
 package com.project.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-
 import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DruidController {
     public String git(){
         SimpleDateFormat simpleDateFormat=new SimpleDateFormat();
+        System.out.println(simpleDateFormat.format(new Date()));
         return null;
     }
 }
